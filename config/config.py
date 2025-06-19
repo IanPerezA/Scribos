47dc68a55fa8c65bfcfd5f86e0346ca562c55f0b
IanPerezA <ipereza1702@alumno.ipn.mx>
2025-06-19 00:38:28 -0600

    yaaaa

--- config/config.py
+++ config/config.py
@@ -10,20 +10,20 @@ class Settings(BaseSettings):
     api_prefix:   str = "/api/v1"
     debug:        bool = False
 
-    MODEL_NAME:      str  = "dccuchile/bert-base-spanish-wwm-cased"
-    LOCAL_MODEL_DIR: Path = Path("modelo_beto")
-    WORDS_FILE:      Path = Path("data/words.txt")
-    INDEX_FILE:      Path = Path("data/index.json")
-    DRIVE_MODEL_URL: str = "https://drive.google.com/file/d/14kk86vRe98SzPP2WqZE7wuIN2eT9whVJ"
+    MODEL_NAME:      str
+    LOCAL_MODEL_DIR: Path
+    WORDS_FILE:      Path
+    INDEX_FILE:      Path
+    DRIVE_MODEL_URL: str
 
     model_config: ClassVar[SettingsConfigDict] = SettingsConfigDict(
-        env_file="C:/Users/HP/Documents/repo-scribos/Scribos/.env",       
+        env_file=".env",  # Esto es ignorado en Railway, pero útil en local
         env_file_encoding="utf-8",
         case_sensitive=False,
     )
 
 
-@lru_cache               
+@lru_cache
 def get_settings() -> Settings:
     return Settings()
 
